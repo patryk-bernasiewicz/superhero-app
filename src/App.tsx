b5a1b6b66c4b14412269154a9ea628e2f587bcb7
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2022-02-26 20:28:22 +0100

    add layout component + tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import { SuperheroContextProvider } from '@context/SuperheroContext';
+import { Layout } from '@components/Layout/Layout';
 
 const App = () => {
-  return <SuperheroContextProvider>Hello World!</SuperheroContextProvider>;
+  return (
+    <SuperheroContextProvider>
+      <Layout>Hello World!</Layout>
+    </SuperheroContextProvider>
+  );
 };
 
 export default App;
